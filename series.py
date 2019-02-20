5fe7641e9025491c122aee4d8c886ad7da0c8d90
Chris Ball <chrisball7@gmail.com>
2019-02-19 16:35:38 -0800

    fixed bad test for sum_series and wrote passing function for sum_series

--- series.py
+++ series.py
@@ -39,3 +39,22 @@ def lucas(n):
     return next_num
 
 # 2, 1, 3, 4, 7, 11, 18, 29, 47, 76 ...
+
+
+def sum_series(n, first=0, second=1):
+    """
+
+    """
+    first = first
+    second = second
+    if n == 1:
+        return first
+    elif n == 2:
+        return second
+    else:
+        for i in range(n - 2):
+            next_num = first + second
+            first, second = second, next_num
+    return next_num
+
+# 4, 9, 13, 22, 35, 57, 92, 149, 241, 390
--- test_series.py
+++ test_series.py
@@ -1,4 +1,4 @@
-from series import fibonacci, lucas
+from series import fibonacci, lucas, sum_series
 
 
 def test_fib_n_at_1():
@@ -64,55 +64,52 @@ def test_lucas_n_at_8():
 def test_both_n_at_1_0_1():
     actual = sum_series(1, 0, 1)
     expected = 0
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_6_0_1():
     actual = sum_series(6, 0, 1)
     expected = 5
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_10_0_1():
     actual = sum_series(10, 0, 1)
     expected = 34
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_1_2_1():
     actual = sum_series(1, 2, 1)
     expected = 2
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_6_2_1():
     actual = sum_series(6, 2, 1)
     expected = 11
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_10_2_1():
     actual = sum_series(10, 2, 1)
     expected = 76
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_1_4_9():
     actual = sum_series(1, 4, 9)
-    expected = 13
-    assert actual = expected
+    expected = 4
+    assert actual == expected
 
 
 def test_both_n_at_6_4_9():
     actual = sum_series(6, 4, 9)
     expected = 57
-    assert actual = expected
+    assert actual == expected
 
 
 def test_both_n_at_10_4_9():
     actual = sum_series(10, 4, 9)
     expected = 390
-    assert actual = expected
-
-
-# 4, 9, 13, 22, 35, 57, 92, 149, 241, 390
+    assert actual == expected
