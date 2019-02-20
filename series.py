ef429577568698a9e5690bc3bcde6c5d075cfa44
Chris Ball <chrisball7@gmail.com>
2019-02-19 16:30:37 -0800

    tests for sum_series written

--- series.py
+++ series.py
@@ -16,8 +16,26 @@ def fibonacci(n):
     else:
         for i in range(n - 2):
             next_num = first + second
-            first = second
-            second = next_num
+            first, second = second, next_num
     return next_num
 
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
+
+def lucas(n):
+    """
+
+    """
+    first = 2
+    second = 1
+    if n == 1:
+        return first
+    elif n == 2:
+        return second
+    else:
+        for i in range(n - 2):
+            next_num = first + second
+            first, second = second, next_num
+    return next_num
+
+# 2, 1, 3, 4, 7, 11, 18, 29, 47, 76 ...
--- test_series.py
+++ test_series.py
@@ -1,31 +1,118 @@
-from series import fibonacci
+from series import fibonacci, lucas
 
 
-def test_check_n_at_1():
+def test_fib_n_at_1():
     actual = fibonacci(1)
     expected = 0
     assert actual == expected
 
 
-def test_check_n_at_2():
+def test_fib_n_at_2():
     actual = fibonacci(2)
     expected = 1
     assert actual == expected
 
 
-def test_check_n_at_3():
+def test_fib_n_at_3():
     actual = fibonacci(3)
     expected = 1
     assert actual == expected
 
 
-def test_check_n_at_4():
+def test_fib_n_at_4():
     actual = fibonacci(4)
     expected = 2
     assert actual == expected
 
 
-def test_check_n_at_8():
+def test_fib_n_at_8():
     actual = fibonacci(8)
     expected = 13
     assert actual == expected
+
+
+def test_lucas_n_at_1():
+    actual = lucas(1)
+    expected = 2
+    assert actual == expected
+
+
+def test_lucas_n_at_2():
+    actual = lucas(2)
+    expected = 1
+    assert actual == expected
+
+
+def test_lucas_n_at_3():
+    actual = lucas(3)
+    expected = 3
+    assert actual == expected
+
+
+def test_lucas_n_at_4():
+    actual = lucas(4)
+    expected = 4
+    assert actual == expected
+
+
+def test_lucas_n_at_8():
+    actual = lucas(8)
+    expected = 29
+    assert actual == expected
+
+
+def test_both_n_at_1_0_1():
+    actual = sum_series(1, 0, 1)
+    expected = 0
+    assert actual = expected
+
+
+def test_both_n_at_6_0_1():
+    actual = sum_series(6, 0, 1)
+    expected = 5
+    assert actual = expected
+
+
+def test_both_n_at_10_0_1():
+    actual = sum_series(10, 0, 1)
+    expected = 34
+    assert actual = expected
+
+
+def test_both_n_at_1_2_1():
+    actual = sum_series(1, 2, 1)
+    expected = 2
+    assert actual = expected
+
+
+def test_both_n_at_6_2_1():
+    actual = sum_series(6, 2, 1)
+    expected = 11
+    assert actual = expected
+
+
+def test_both_n_at_10_2_1():
+    actual = sum_series(10, 2, 1)
+    expected = 76
+    assert actual = expected
+
+
+def test_both_n_at_1_4_9():
+    actual = sum_series(1, 4, 9)
+    expected = 13
+    assert actual = expected
+
+
+def test_both_n_at_6_4_9():
+    actual = sum_series(6, 4, 9)
+    expected = 57
+    assert actual = expected
+
+
+def test_both_n_at_10_4_9():
+    actual = sum_series(10, 4, 9)
+    expected = 390
+    assert actual = expected
+
+
+# 4, 9, 13, 22, 35, 57, 92, 149, 241, 390
